d8bd79629403eb5ec65ad2ee4a1e5908d236c9b5
unknown <danenkov.dmitriy@list.ru>
2022-07-28 22:29:57 +0300

    hometask6

---
+++ Homework6classes/Homework6classes/Mobile.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework6classes
+{
+    public class Mobile
+    {
+        private double _weight;
+        public Mobile(string number, string model, double weight)
+        {
+            Weight = weight;
+            Model = model;
+            Number = number;
+        }
+       
+
+       
+        public double Weight  
+        {
+            get
+            {
+                return _weight;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _weight = 0;
+                }
+                else
+                {
+                    _weight = value;
+                }
+
+            }
+
+        }
+
+        public string Model
+        {
+            get;
+            private set;
+        }
+        public string Number
+        {
+            get;
+            set;
+        }
+        
+
+        public void RecieveCall (string zvonok)
+        {
+            Console.WriteLine($"{zvonok} is calling you....");
+        }
+        
+        public void SuperLoto (int index)
+        {
+            var random = new Random();
+            var list = new List<string> { "Iphone", "Motorolla", "Pocophone", "Nokia" };
+            index = random.Next(list.Count);
+            Console.WriteLine(list[index]);
+
+        }
+
+
+    }
+}
---
+++ Homework6classes/Homework6classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework6classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index;
+            Mobile Motorolla = new Mobile("+375293334455", "zx9", 249.99);
+            Mobile Pokophone = new Mobile("+375293332211", "POCO9", 189.5);
+            Console.WriteLine($"Weight of device1: {Motorolla.Weight}");
+            Console.WriteLine($"Weight of device2:{Pokophone.Weight}");
+            Console.WriteLine($"Model of device 1:{Motorolla.Model}");
+            Console.WriteLine($"Model of device 2 :{Pokophone.Model}");
+            Motorolla.Weight = -212;
+            Console.WriteLine(Motorolla.Weight);
+            Motorolla.RecieveCall("Plotva");
+            //random choose of mobile; Позже в методы перекину
+            var random = new Random();
+            var list = new List<string> { "Iphone", "Motorolla", "Pocophone", "Nokia" };
+            index = random.Next(list.Count);
+            Console.WriteLine(list[index]);
+        }
+    }
+}
